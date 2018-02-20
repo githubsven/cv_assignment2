049bfa3e0c3d507d0cca160632ff6cdab12a1c04
Sven Luehof <s.luehof@gmail.com>
2018-02-20 09:41:33 +0100

    Initial

---
+++ cv_assignment2/cv_assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cv_assignment2
+//
+//  Created by Sven Luehof on 20/02/2018.
+//  Copyright © 2018 Sven Luehof. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
