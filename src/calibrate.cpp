84570c59130eb448521ab184871ebe5801f5e523
githubsven <s.luehof@gmail.com>
2018-02-22 12:41:47 +0100

    Added calibrate file

---
+++ src/calibrate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+static void runCalibration(Size& imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point3f>> objectPoints,
+	vector<vector<Point2f> > imagePoints, vector<Mat>& rvecs, vector<Mat>& tvecs,
+	vector<float>& reprojErrs, double& totalAvgErr)
+{
+	cameraMatrix = Mat::eye(3, 3, CV_64F);
+	if (CV_CALIB_FIX_ASPECT_RATIO)
+		cameraMatrix.at<double>(0, 0) = 1.0;
+
+	distCoeffs = Mat::zeros(8, 1, CV_64F);
+
+	//objectPoints.resize(imagePoints.size(), objectPoints[0]);
+	//Find intrinsic and extrinsic camera parameters
+	double rms = calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix,
+		distCoeffs, rvecs, tvecs, CV_CALIB_FIX_K4 | CV_CALIB_FIX_K5);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 
 #include "utilities/General.h"
 #include "VoxelReconstruction.h"
+#include "calibrate.h"
 
 using namespace nl_uu_science_gmt;
 
 int main(
 		int argc, char** argv)
 {
+	calibrate::runCalibration();
 	VoxelReconstruction::showKeys();
 	VoxelReconstruction vr("data" + std::string(PATH_SEP), 4);
 	vr.run(argc, argv);
